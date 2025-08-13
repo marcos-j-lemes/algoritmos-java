507504c5ef903e49d904485cffd89501f66f639b
marcos-j-lemes <marcos.jlf@aluno.ifsc.edu.br>
2025-08-13 18:20:05 -0300

    feat: settings main

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,15 @@ public class Main{
 
     public static void main(String[] args){
         System.out.println("Hello, world");
+
+        teste(100);
+    }
+
+
+    public static void teste(int x){
+
+        for(int i = 0; i < x; i++){
+            System.out.println("Hello, world");
+        }
     }
 }
